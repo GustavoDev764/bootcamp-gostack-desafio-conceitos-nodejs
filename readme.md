5c2d48e44c630ca1d22960efbddb50df1424141b
GustavoDev764 <GustavoDev764@gmail.com>
2020-08-17 17:15:54 -0300

    Salva a api

---
+++ readme.md
@@ -0,0 +1,18 @@
+<h2 align="center">
+  Desafio 02: Conceitos do Node.js
+</h2>
+
+<p align="center">
+  <img alt="GitHub language count" src="photos/languages.svg">
+
+  <img alt="GitHub top language" src="photos/languages2.svg">
+
+  <img alt="License" src="photos/license.svg">
+
+</p>
+
+## 🤓 Sobre o desafio
+
+Nesse desafio, você deve criar uma aplicação para treinar o que você aprendeu até agora no Node.js!
+
+Essa será uma aplicação para armazenar repositórios do seu portfólio, que irá permitir a criação, listagem, atualização e remoção dos repositórios, e além disso permitir que os repositórios possam receber "likes".
--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +9,67 @@ app.use(cors());
 
 const repositories = [];
 
+//Lista
 app.get("/repositories", (request, response) => {
-  // TODO
+  response.status(200).json(repositories);
 });
 
+//Cria
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { url, title, techs } = request.body;
+
+  const repositorie = { id: uuid(), title, url, techs, likes: 0 };
+  repositories.push(repositorie);
+  response.status(200).json(repositorie);
 });
 
+//Atualiza
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { url, title, techs } = request.body;
+
+  let item = repositories.find((item) => item.id == id);
+  const itemIndex = repositories.findIndex((item) => item.id == id);
+
+  if (itemIndex < 0) {
+    return response.status(400).json({ error: "Elemento não encontrado" });
+  }
+
+  item = { ...item, url, title, techs };
+
+  repositories[itemIndex] = item;
+
+  response.status(200).json(item);
 });
 
+//Deleta
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const itemdelete = repositories.findIndex((item) => item.id == id);
+  if (itemdelete < 0) {
+    return response.status(400).json({ error: "Elemento não encontrado" });
+  }
+  repositories.splice(itemdelete, 1);
+
+  response.status(204).send();
 });
 
+//Add Like
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const itemIndex = repositories.findIndex((item) => item.id == id);
+
+  if (itemIndex < 0) {
+    return response.status(400).json({ error: "Elemento não encontrado" });
+  }
+
+  let item = repositories.find((item) => item.id == id);
+  item.likes += 1;
+
+  repositories[itemIndex] = item;
+  return response.status(200).json(item);
 });
 
 module.exports = app;
